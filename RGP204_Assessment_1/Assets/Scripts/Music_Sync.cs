b650f7970e86f9c2a32a46ba770b69973d671326
Skilious <ta_iha99@outlook.com>
2020-06-04 16:52:31 +1000

    First push.

---
+++ RGP204_Assessment_1/Assets/Scripts/Music_Sync.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music_Sync : MonoBehaviour
+{
+    float songPosition;
+    float songPosInBeats;
+
+    float secPerBeat;
+    float dspTimeSong;
+
+    float bpm;
+    void Start()
+    {
+        //calculate how many seconds is one beat
+        //declaration of bpm later
+        secPerBeat = 60f / bpm;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
